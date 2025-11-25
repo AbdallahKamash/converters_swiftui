e696b2b99b920cc66c7426225a0c17bda4b4785b
Abdallah Kamash <abdallah-kamash@outlook.com>
2025-11-25 06:14:00 +0200

    Initial Commit

---
+++ converters/convertersApp.swift
@@ -0,0 +1,17 @@
+//
+//  convertersApp.swift
+//  converters
+//
+//  Created by Abdallah Kamash on 25/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct convertersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ convertersTests/convertersTests.swift
@@ -0,0 +1,16 @@
+//
+//  convertersTests.swift
+//  convertersTests
+//
+//  Created by Abdallah Kamash on 25/11/2025.
+//
+
+import Testing
+
+struct convertersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
